68edaa386c122357e056b5e4f02f0585db0e4b25
Marcy, Kyle <k.marcy@northeastern.edu>
2018-10-16 16:11:37 -0400

    integrated the staff page js

--- wp-content/themes/nudev/src/js/scripts.js
+++ wp-content/themes/nudev/src/js/scripts.js
@@ -1,160 +1,278 @@
+var exceedsContainer = false;
 (function ($, root, undefined) {
 
-	$(function () {
+    $(function () {
 
-		if ($(window).width() < 1260) {
-   		$('.download').removeClass('hvr-shutter-out-horizontal');
-		}
-		else {
-		   $('.download').addClass('hvr-shutter-out-horizontal');
-		}
-
-		//$('.download').removeClass('hvr-shutter-out-horizontal');
-
-
-
-
-
-		// if we are on the staff page, allow for some links to open full bio details in a lightbox
-		if($("#staff").length){
-
-			$(".js__bio").magnificPopup({
-	        // type: "iframe"
-					type: "ajax"
-	        ,closeOnContentClick: false
-	        ,closeOnBgClick: false
-	        ,enableEscapeKey: false
-	        ,verticalFit: true
-					,removalDelay: 300
-					,mainClass: 'mfp-fade'
-					// ,callbacks:{
-					//   beforeOpen: function() {
-					//     console.log('Start of popup initialization');
-					//   }
-					// }
-	    });
-
-
-
-		}
-
-
-
-
-
-
-		// the following deals with the auto scrolling of long pages
-		var pageSections = {};
-		var targetOffset = 0;
-
-		targetOffset = ($(window).height() / 2);  // half the height of the browser window, needs to be re-calculated on page re-size!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    //console.log(targetOffset);
-
-		//console.log("HEADER: "+$("header").height());
-
-		function getScrollSections(){
-			$(".nu__section-break").each(function(i){
-				if(i == 0){
-					pageSections[$(this).attr("id")] = 0;
-				}else{
-					pageSections[$(this).attr("id")] = parseInt(($(this).offset().top) + $("header").height() - targetOffset);
-				}
-	    });
-		}
-
-		getScrollSections();
-		//console.log(pageSections);
-
-
-    // this is the function that will check if the element is on the screen or not
-    function isScrolledIntoView(elem){
-      var docViewTop = $(window).scrollTop();
-      var docViewBottom = docViewTop + $(window).height();
-      var elemTop = $(elem).offset().top;
-      var elemBottom = elemTop + $(elem).height();
-      return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-    }
-
-    $(window).scroll(function(){
-			$(".nu__col-left ul:not(.noautoscroll) li a").removeClass("active");
-			var i = 0;
-			for(var p in pageSections){
-        result = isScrolledIntoView($("#section-"+i));
-
-				if(result){
-					//console.log("SECTION-"+i+": "+result);
-					$(".nu__col-left ul li a").removeClass("active");
-					$(".nu__col-left ul li a[data-id="+i+"]").addClass("active");
-					break;
-				}
-				i++;
-      }
-    });
-
-		// when a user clicks on one of the sub-nav options to scroll the page
-		$(".nu__col-left ul:not(.noautoscroll)").on("click", "a",function(e){
-			console.log("click");
-			e.preventDefault();
-			var t = $(this);
-			$(".nu__col-left ul li a").removeClass("active");
-			$("html,body").animate({
-          scrollTop: pageSections["section-"+t.attr("data-id")]
-      },500,function(){
-				t.addClass("active");
-			});
-		});
-
-
-
-		/*Remove the lines below once you are done testing*/
-var wi = $(window).width();
-$("p.testp").text('Screen width is currently: ' + wi + 'px.');
-
-$(window).resize(function() {
-
-
-
-	targetOffset = ($(window).height() / 2);
-
-	getScrollSections();
-
-	if ($(window).width() < 1260) {
-		$('.download').removeClass('hvr-shutter-out-horizontal');
-	}
-	else {
-		 $('.download').addClass('hvr-shutter-out-horizontal');
-	}
-
-
-
-    /*Remove the lines below once you are done testing*/
-    var wi = $(window).width();
-
-    if (wi <= 576){
-        $("p.testp").text('Screen width is less than or equal to 576px. Width is currently: ' + wi + 'px.');
+        if ($(window).width() < 1260) {
+            $('.download').removeClass('hvr-shutter-out-horizontal');
+        } else {
+            $('.download').addClass('hvr-shutter-out-horizontal');
         }
-    else if (wi <= 680){
-                                    $("p.testp").text('Screen width is between 577px and 680px. Width is currently: ' + wi + 'px.');
-                                    }
-    else if (wi <= 1024){
-                                    $("p.testp").text('Screen width is between 681px and 1024px. Width is currently: ' + wi + 'px.');
-                                    }
-    else if (wi <= 1500){
-                                    $("p.testp").text('Screen width is between 1025px and 1499px. Width is currently: ' + wi + 'px.');
-                                    }
-    else {
-                                    $("p.testp").text('Screen width is greater than 1500px. Width is currently: ' + wi + 'px.');
-                                    }
-});
+
+        //$('.download').removeClass('hvr-shutter-out-horizontal');
+
+
+
+
+
+        // if we are on the staff page, allow for some links to open full bio details in a lightbox
+        if ($("#staff").length) {
+
+            $(".js__bio").magnificPopup({
+                // type: "iframe"
+                type: "ajax",
+                closeOnContentClick: false,
+                closeOnBgClick: false,
+                enableEscapeKey: false,
+                verticalFit: true,
+                removalDelay: 300,
+                mainClass: 'mfp-fade'
+                // ,callbacks:{
+                //   beforeOpen: function() {
+                //     console.log('Start of popup initialization');
+                //   }
+                // }
+            });
+
+
+
+        }
 
 
 
 
 
 
+        // the following deals with the auto scrolling of long pages
+        var pageSections = {};
+        var targetOffset = 0;
+
+        targetOffset = ($(window).height() / 2); // half the height of the browser window, needs to be re-calculated on page re-size!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        //console.log(targetOffset);
+
+        //console.log("HEADER: "+$("header").height());
+
+        function getScrollSections() {
+            $(".nu__section-break").each(function (i) {
+                if (i == 0) {
+                    pageSections[$(this).attr("id")] = 0;
+                } else {
+                    pageSections[$(this).attr("id")] = parseInt(($(this).offset().top) + $("header").height() - targetOffset);
+                }
+            });
+        }
+
+        getScrollSections();
+        //console.log(pageSections);
+
+
+        // this is the function that will check if the element is on the screen or not
+        function isScrolledIntoView(elem) {
+            var docViewTop = $(window).scrollTop();
+            var docViewBottom = docViewTop + $(window).height();
+            var elemTop = $(elem).offset().top;
+            var elemBottom = elemTop + $(elem).height();
+            return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+        }
+
+        $(window).scroll(function () {
+            $(".nu__col-left ul:not(.noautoscroll) li a").removeClass("active");
+            var i = 0;
+            for (var p in pageSections) {
+                result = isScrolledIntoView($("#section-" + i));
+
+                if (result) {
+                    //console.log("SECTION-"+i+": "+result);
+                    $(".nu__col-left ul li a").removeClass("active");
+                    $(".nu__col-left ul li a[data-id=" + i + "]").addClass("active");
+                    break;
+                }
+                i++;
+            }
+        });
+
+        // when a user clicks on one of the sub-nav options to scroll the page
+        $(".nu__col-left ul:not(.noautoscroll)").on("click", "a", function (e) {
+            console.log("click");
+            e.preventDefault();
+            var t = $(this);
+            $(".nu__col-left ul li a").removeClass("active");
+            $("html,body").animate({
+                scrollTop: pageSections["section-" + t.attr("data-id")]
+            }, 500, function () {
+                t.addClass("active");
+            });
+        });
 
 
 
-	});
+        /*Remove the lines below once you are done testing*/
+        var wi = $(window).width();
+        $("p.testp").text('Screen width is currently: ' + wi + 'px.');
+
+        $(window).resize(function () {
+
+            // if we are NOT on the homepage, kick off a filter check
+			if(!$('body').hasClass('home') && $('.nu__filters').length > 0){
+				filterNavCheck();	// check to see what needs to be shown and what is overflow for filters
+				hideMoreFilters();	// hide the additional filters if they are visible
+			}
+
+            targetOffset = ($(window).height() / 2);
+
+            getScrollSections();
+
+            if ($(window).width() < 1260) {
+                $('.download').removeClass('hvr-shutter-out-horizontal');
+            } else {
+                $('.download').addClass('hvr-shutter-out-horizontal');
+            }
+
+
+
+            /*Remove the lines below once you are done testing*/
+            var wi = $(window).width();
+
+            if (wi <= 576) {
+                $("p.testp").text('Screen width is less than or equal to 576px. Width is currently: ' + wi + 'px.');
+            } else if (wi <= 680) {
+                $("p.testp").text('Screen width is between 577px and 680px. Width is currently: ' + wi + 'px.');
+            } else if (wi <= 1024) {
+                $("p.testp").text('Screen width is between 681px and 1024px. Width is currently: ' + wi + 'px.');
+            } else if (wi <= 1500) {
+                $("p.testp").text('Screen width is between 1025px and 1499px. Width is currently: ' + wi + 'px.');
+            } else {
+                $("p.testp").text('Screen width is greater than 1500px. Width is currently: ' + wi + 'px.');
+            }
+        });
+
+
+
+        // this function will check filter navs used on pages to see if the items exceed the width of the container
+		function filterNavCheck(){
+
+			if($(window).width() <= 620){	// we are on a much smaller screen, so ignore the more option and stack via CSS
+				// console.log('screen size less than 620px');
+				$('.nu__filters > div > div').hide();
+				$('.nu__filters > div > ul > li').removeAttr('style');
+				$('.nu__filters > div > ul > li').removeClass('inshowmore');
+				exceedsContainer = false;
+			}else{	// we need to use the more option to allow user to see all options
+
+				// $('.nu__filters > div > div').show();	// we need to show the more option
+
+				var offset = 0;
+				var filterWidth = $('.nu__filters > div > ul').width();
+
+				// total up the width of all of the filter options
+				var itemWidth = 0;
+				var tPos = $('.nu__filters > div > ul > li').first().position().top;
+				//console.log(tPos);
+				var vOffset = ($('.nu__filters').height() - 2);
+
+				// console.log(vOffset);
+
+				// $('.nu__filters > div > ul > li.inshowmore').removeAttr('style');
+				// $('.nu__filters > div > ul > li.inshowmore').removeClass('inshowmore');
+
+				// $('.nu__filters > div > ul > li.inshowmore').removeAttr('style');
+				$('.nu__filters > div > ul > li.inshowmore').removeClass('inshowmore');
+
+				$('.nu__filters > div > ul > li > a').each(function(i){
+					itemWidth += $(this).outerWidth();
+					//console.log($(this).parent().position().top);
+					// if($(this).parent().position().top > tPos){
+					if(itemWidth > filterWidth){
+						//console.log($(this));
+						$(this).parent().addClass('inshowmore').css({'top':vOffset});
+						vOffset += $(this).parent().height();
+					}else{
+
+						// need to re-check the position vs width here to remove styles if no longer hiding
+
+						// $('.nu__filters > div > ul > li.inshowmore').removeAttr('style');
+						// $('.nu__filters > div > ul > li.inshowmore').removeClass('inshowmore');
+						// // $(this).parent().removeClass('inshowmore').css({'top':vOffset});
+						// vOffset -= $(this).parent().height();
+						$(this).parent().removeAttr('style');
+						$(this).parent().removeClass('inshowmore');
+					}
+				});
+
+				// now let's figure out if the content fits inside the container or not
+				if((itemWidth + offset) >= filterWidth){
+					if(!exceedsContainer){
+						// console.log('content exceeds container!');
+						exceedsContainer = true;
+
+						// let's show the more button as the items do not fit
+						$('.nu__filters > div > div').show();
+
+					}
+				}else if((itemWidth + offset) < filterWidth){
+					if(exceedsContainer){
+						// console.log('content fits within container again!');
+						exceedsContainer = false;
+
+						// more than enough room, hide the more button
+						$('.nu__filters > div > div').hide();
+
+						$('.nu__filters > div > ul > li.inshowmore').removeAttr('style');
+						$('.nu__filters > div > ul > li.inshowmore').removeClass('inshowmore');
+
+					}
+				}
+			}
+		}
+
+		// if we are NOT on the homepage, kick off a filter check right away
+		if(!$('body').hasClass('home') && $('.nu__filters').length > 0){
+			filterNavCheck();
+		}
+
+
+
+
+        function showMoreFilters(){
+			if(!$('.js__showmore').hasClass('active')){
+				$('.js__showmore').addClass('active');
+				$('.nu__filters > div > ul > li.inshowmore').each(function(){
+					$(this).css({'opacity':'1.0'});
+				});
+				$('.nu__filters').css({'overflow':'visible'});
+			}
+		}
+
+		function hideMoreFilters(){
+			if($('.js__showmore').hasClass('active')){
+				$('.js__showmore').removeClass('active');
+				$('.nu__filters > div > ul > li.inshowmore').each(function(){
+					$(this).css({'opacity':'0.0'});
+				});
+				$('.nu__filters').css({'overflow':'hidden'});
+			}
+        }
+        
+        // this will handle clicking on the more button in filter options
+		$('.nu__filters').on('click','.js__showmore',function(e){
+
+			// console.log('I would like to see more!');
+
+			// showHideMore();
+
+			// $('.nu__filters ul').height('auto');
+
+			if(!$(this).hasClass('active')){
+				showMoreFilters();
+			}else{
+				hideMoreFilters();
+			}
+
+        });
+        
+
+
+
+    });
 
 })(jQuery, this);
