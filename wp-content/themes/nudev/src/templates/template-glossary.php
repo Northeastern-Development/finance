267d2c5f39fc0a5b7ea465812a1c39b752874386
Marcy, Kyle <k.marcy@northeastern.edu>
2018-10-26 10:37:44 -0400

    tweak

---
+++ wp-content/themes/nudev/src/templates/template-glossary.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * Template Name: Glossary
+ */
+    get_header();
+ ?>
+<main role="main">
+    <section>
+
+        
+        
+        
+    </section>
+</main>
+<?php 
+    get_footer();
+ ?>
