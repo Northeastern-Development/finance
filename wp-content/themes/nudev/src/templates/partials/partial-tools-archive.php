a86e2ca0e3ca43260a9974bfed9cf422998c04c8
Marcy, Kyle <k.marcy@northeastern.edu>
2018-10-31 15:03:11 -0400

    building the tools single template up

--- wp-content/themes/nudev/src/templates/partials/partial-tools-archive.php
+++ wp-content/themes/nudev/src/templates/partials/partial-tools-archive.php
@@ -13,30 +13,48 @@
     );
     $tools = get_posts($args);
 
-    $content = '';
-    $guide = '<p><a href="%s" title="More Information about (toolname)" target="_blank">%s</a></p>';
+    $content = '<ul class="tools-grid">';
 
+
+    $guide ='
+        <li class="tools-grid-tool">
+            <a href="%s" target="_blank">
+                <figure>
+                    <img src="%s" alt="">
+                </figure>
+                <h5>%s</h5>
+                <h6>%s</h6>
+                <p>%s</p>
+                <p>Learn More</p>
+            </a>
+        </li>
+    ';
+
+    // [0] => status [1] => categories [2] => image [3] => sub_title [4] => short_description [5] => full_description [6] => custom_link [7] => external_link [8] => groupings 
     foreach( $tools as $post ){
+
         $fields = get_fields($post);
 
         $content .= sprintf(
             $guide
             ,get_permalink($post) . seoUrl($fields['groupings'][0]['title'])
+            ,wp_get_attachment_image_src($fields['image'])[0]
             ,$post->post_title
+            ,$fields['sub_title']
+            ,$fields['short_description']
         );
-        
+
     }
+    $content .= '</ul>';
+
+
 
 
     get_header();
  ?>
 <main id="tools">
     <section>
-
-
         <?php echo $content; ?>
-        
-        
     </section>
 </main>
 <?php 
--- wp-content/themes/nudev/src/templates/partials/partial-tools-single.php
+++ wp-content/themes/nudev/src/templates/partials/partial-tools-single.php
@@ -1,29 +1,60 @@
 <?php 
-
-    // (these exist already in the parent template)
-    // $isPost = $wp_query->query_vars['toolname'];
-    // $theGrouping = $wp_query->query_vars['toolgroup'];
-
-
     $toolPost = get_page_by_path($isPost, ARRAY_A, 'tools');
     $fields = get_fields($toolPost['ID']);
+
+    // pre-set an object to contain the fields for the current grouping
+    // when the grouping nav is clicked, it will refresh the page; rewriting the current grouping; which will cause this object to dynamically change
+    // this should always have an array of the subfields inside of the 'current' grouping
+    $currentGrouping = [];
     
-    $content = '<ul>';
-    $guide = '<li><a href="%s">%s</a></li>';
 
+    $groupSelectorGuide = '<li><a href="%s">%s</a></li>';
+    $groupSelectorContent = '<ul class="tool-groupingnav">';
     foreach( $fields['groupings'] as $grouping ){
-
-        $content .= sprintf(
-            $guide
+        // pass title to both the nav and the list
+        $groupSelectorContent .= sprintf(
+            $groupSelectorGuide
             ,get_permalink($toolPost['ID']) . seoUrl($grouping['title'])
             ,$grouping['title']
         );
+        if( strtolower($grouping['title']) == $theGrouping  ){
+            $currentGrouping = $grouping;
+        }
+    }
+    $groupSelectorContent .= '</ul>';
+
+
+
+    // (main page content stuff here)
+    // we need a UL for each group - a line item for each info block, 
+    
+    $groupContent = '<ul>';
+
+    $groupGuide = '<h6>%s</h6><li>%s</li>';
+
+    
+    $infoblockGuide = '<li><p>%s</p><p>%s</p></li>';
+    
+    foreach( $currentGrouping['group'] as $group ){
+        // prep the infoblock content var
+        $infoblockContent = '';
+        // set the infoblock content var
+        foreach( $group['information_blocks'] as $infoblock ){
+            $infoblockContent .= sprintf(
+                $groupGuide
+                ,$infoblock['title']
+                ,$infoblock['description']
+            );
+        }
+        // sprint the 'group' and its infoblock within the outer grouping
+        $groupContent .= sprintf(
+            $groupGuide
+            ,$group['title']
+            ,$infoblockContent
+        );
         
     }
-    $content .= '</ul>';
-
-
-
+    $groupContent .= '</ul>';
 
     get_header();
 ?>
@@ -31,15 +62,10 @@
     <section>
         <?php 
 
-
-
-            echo $content;
-
-            // [0] => status [1] => categories [2] => image [3] => sub_title [4] => short_description [5] => full_description [6] => custom_link [7] => external_link [8] => groupings 
+            echo $groupSelectorContent;
+            echo $groupContent;
 
          ?>
-        
-        
     </section>
 </main>
 <?php 
