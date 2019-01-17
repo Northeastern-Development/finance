f82463481ebbc4f4bc0d3800ba25229ce849f3d3
Marcy, Kyle <k.marcy@northeastern.edu>
2019-01-17 17:09:30 -0500

    making a final pass; page by page; for good enough status for review by sean

--- wp-content/themes/nudev/src/loops/loop-departments.php
+++ wp-content/themes/nudev/src/loops/loop-departments.php
@@ -16,47 +16,47 @@
     );
     $res = get_posts($args);
 
-    if( get_page_template_slug($post_id) == 'templates/template-about.php' ){
-        $format_depts = '
-            <li>
-                <a href="%s" title="Click to view department">
-                    <h3>%s</h3>
-                    <div class="neu__bgimg">
-                        <div style="background-image: url(%s)"></div>
-                    </div>
-                </a>
-            </li>
-        ';
-        foreach ($res as $rec) {
-            $fields = get_fields($rec->ID);
-            $content_depts .= sprintf(
-                $format_depts,
-                get_permalink($rec->ID),
-                $rec->post_title,
-                $fields['featured_image']
-            );
+    if( !empty($res) ){
+        if( get_page_template_slug($post_id) == 'templates/template-about.php' ){
+            $format_depts = '
+                <li>
+                    <a href="%s" title="Click to view department">
+                        <h3>%s</h3>
+                        <div class="neu__bgimg">
+                            <div style="background-image: url(%s)"></div>
+                        </div>
+                    </a>
+                </li>
+            ';
+            foreach ($res as $rec) {
+                $fields = get_fields($rec->ID);
+                $content_depts .= sprintf(
+                    $format_depts,
+                    get_permalink($rec->ID),
+                    $rec->post_title,
+                    $fields['featured_image']
+                );
+            }
+        }
+        else if( get_page_template_slug($post_id) == 'templates/template-contact.php' ){
+    
+            $format_depts = '
+                <li>
+                    <a class="neu__iconlink" href="%s" title="Click to view %s Department">%s</a>
+                </li>
+            ';
+    
+            foreach( $res as $rec ){
+                $content_depts .= sprintf(
+                    $format_depts
+                    ,get_permalink($rec->ID)
+                    ,$rec->post_title
+                    ,$rec->post_title
+                );
+            }
+    
         }
     }
-    else if( get_page_template_slug($post_id) == 'templates/template-contact.php' ){
-
-        $format_depts = '
-            <li>
-                <a class="neu__iconlink" href="%s" title="Click to view %s Department">%s</a>
-            </li>
-        ';
-
-        foreach( $res as $rec ){
-            $content_depts .= sprintf(
-                $format_depts
-                ,get_permalink($rec->ID)
-                ,$rec->post_title
-                ,$rec->post_title
-            );
-        }
-
-    }
-    
-    
 ?>
 <ul>
     <?= $content_depts; ?>
